a7db84b4f1f5d2a51dbdb1f129db57bbb9418a14
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-24 23:34:40 +0530

    Added: Get number of contacts ie count by city or state

--- src/com/bridgelabz/AddressBookManagement.java
+++ src/com/bridgelabz/AddressBookManagement.java
@@ -11,13 +11,13 @@
 
 package com.bridgelabz;
 
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AddressBookManagement {
     static Scanner sc = new Scanner(System.in);
     public static Map<String, AddressBookMain> nameToAddressBookMap = new TreeMap<>();
+    public static Map<String, Contact> cityToContactEntryMap = new TreeMap<>();
+    public static Map<String, Contact> stateToContactEntryMap = new TreeMap<>();
 
     /**
      * This method is used to add new Address book
@@ -32,17 +32,132 @@ public class AddressBookManagement {
     }
 
     /**
+     * This method is used to view persons by City
+     */
+    public static void showContactGroupedByCity() {
+        Set<String> listOfCity = cityToContactEntryMap.keySet();
+        for (String city : listOfCity) {
+            System.out.println("Contact Entries for CITY: " + city);
+            searchPersonInBook(city);
+        }
+    }
+
+    /**
+     * This method is used to view persons by State
+     */
+    public static void showContactGroupedByState() {
+        Set<String> listOfState = stateToContactEntryMap.keySet();
+        for (String state : listOfState) {
+            System.out.println("Contact Entries for STATE: " + state);
+            searchPersonInBook(state);
+        }
+    }
+
+    /**
+     * This method is used to search the person
+     * across multiple address book.
+     *
+     * @param search
+     */
+    public static void searchPersonInBook(String search) {
+        int numberOfPerson = 0;
+        Iterator contactArray = nameToAddressBookMap.entrySet().iterator();
+        while (contactArray.hasNext()) {
+            Map.Entry entry = (Map.Entry) contactArray.next();
+            AddressBookMain a = (AddressBookMain) entry.getValue();
+            List<Contact> list = a.getContactArray();
+            for (Contact contact : list) {
+                if (contact.getCity().equals(search) || contact.getState().equals(search)) {
+                    System.out.println(contact);
+                    numberOfPerson++;
+                }
+            }
+            if (numberOfPerson == 0)
+                System.out.println("No person was found");
+        }
+    }
+
+    /**
+     * This method is used to get number of contact persons
+     * i.e count by city
+     */
+    public static void countByCity() {
+        Set<String> listOfCity = cityToContactEntryMap.keySet();
+        for (String cityName : listOfCity) {
+            Contact contactNumber = cityToContactEntryMap.get(cityName);
+            System.out.println("No of contact entries for CITY " + cityName + " " + ((Map<String, AddressBookMain>) contactNumber).size());
+        }
+    }
+
+    /**
+     * This method is used to get number of contact persons
+     * i.e count by state
+     */
+    public static void countByState() {
+        Set<String> listOfState = stateToContactEntryMap.keySet();
+        for (String stateName : listOfState) {
+            Contact contactNumber = stateToContactEntryMap.get(stateName);
+            System.out.println("No of contact entries for STATE " + stateName + " " + ((Map<String, AddressBookMain>) contactNumber).size());
+        }
+    }
+
+
+    /**
      * This is the main method.
      *
      * @param args
      */
     public static void main(String args[]) {
         AddressBookManagement aBookManager = new AddressBookManagement();
-        System.out.println("Enter 1 to add a new BOOK");
-        int choice = sc.nextInt();
-        if (choice == 1)
-            aBookManager.addNewAddressBook();
-        else
-            System.out.println("Invalid choice");
+        while (true) {
+            System.out.println("\n1. Add a new Address Book");
+            System.out.println("\n2. Search person across all address books");
+            System.out.println("\n3. Show contact  by city");
+            System.out.println("\n4. Show contacts by state");
+            System.out.println("\n5. count contacts  by city");
+            System.out.println("\n6. count contacts by state");
+            System.out.println("\n7. Exit");
+            System.out.println("\n Please Enter your choice");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    while (true) {
+                        AddressBookMain m = new AddressBookMain();
+                        System.out.println("Enter A name for address Book");
+                        String name = sc.next();
+                        if (aBookManager.nameToAddressBookMap.containsKey(name)) {
+                            System.out.println("\nAddress Book already exists !!!\n");
+                            continue;
+                        } else {
+                            aBookManager.nameToAddressBookMap.put(name, m);
+                            System.out.println("\n In Address Book : " + name);
+                            m.maintainAddressBook();
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Enter city or state to search a person");
+                    String search = sc.next();
+                    aBookManager.searchPersonInBook(search);
+                    break;
+                case 3:
+                    showContactGroupedByCity();
+                    break;
+                case 4:
+                    showContactGroupedByState();
+                    break;
+                case 5:
+                    countByCity();
+                    break;
+                case 6:
+                    countByState();
+                    break;
+                case 7:
+                    System.exit(0);
+                    break;
+            }
+        }
     }
 }
