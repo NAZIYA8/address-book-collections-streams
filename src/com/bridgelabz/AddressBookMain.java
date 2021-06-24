956ecdd6297ef4b896f76f0f6a8dd2929e82e951
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-24 21:51:55 +0530

    Added: Delete Contact in the address book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -8,7 +8,6 @@
  * @since 22-06-2021
  * ****************************************************************************
  */
-
 package com.bridgelabz;
 
 import java.util.Scanner;
@@ -23,12 +22,39 @@ public class AddressBookMain {
     }
 
     /**
+     * This is the main method.
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
+        AddressBookMain addressBookContacts = new AddressBookMain();
+        System.out.println("Enter number of contact details to enter:");
+        int numOfContact = sc.nextInt();
+        for (int i = 0; i < numOfContact; i++) {
+            System.out.print("Enter FirstName,Last Name,address,city,state,pinCode,phoneNumber,email");
+            String firstName = sc.next();
+            String lastName = sc.next();
+            String address = sc.next();
+            String city = sc.next();
+            String state = sc.next();
+            int pinCode = sc.nextInt();
+            long phoneNumber = sc.nextLong();
+            String emailId = sc.next();
+            addressBookContacts.addNewContact(firstName, lastName, address, city, state, pinCode, phoneNumber, emailId);
+        }
+        addressBookContacts.editContact();
+        System.out.println("Enter first name and last name of contact to be deleted: ");
+        String firstName = sc.next();
+        String lastName = sc.next();
+        addressBookContacts.deleteContactDetails(firstName, lastName);
+    }
+
+    /**
      * Adds new Contact to Address book that has a unique name
      * Display contact detail and Address book name
      */
-
-    private void addNewContact(String firstName, String lastName, String address, String city, String state, int pinCode, long mobiileNumber, String emailId) {
-        contactArray[numOfContact] = new Contact(firstName, lastName, address, city, state, pinCode, mobiileNumber, emailId);
+    private void addNewContact(String firstName, String lastName, String address, String city, String state, int pinCode, long phoneNumber, String emailId) {
+        contactArray[numOfContact] = new Contact(firstName, lastName, address, city, state, pinCode, phoneNumber, emailId);
         System.out.println(contactArray[numOfContact]);
         numOfContact++;
     }
@@ -40,7 +66,6 @@ public class AddressBookMain {
         System.out.println(contactArray);
     }
 
-
     /**
      * Edit existing contact using their name
      */
@@ -50,19 +75,19 @@ public class AddressBookMain {
         String lastName = sc.next();
         for (int i = 0; i < numOfContact; i++) {
             if (contactArray[i].firstName.equals(firstName) && contactArray[i].lastName.equals(lastName)) {
-                System.out.print("Enter address,city,state,pinCode,phoneNumber,email");
-                String address = sc.next();
-                contactArray[i].setAddress(address);
-                String city = sc.next();
-                contactArray[i].setCity(city);
-                String state = sc.next();
-                contactArray[i].setState(state);
-                int pinCode = sc.nextInt();
-                contactArray[i].setPinCode(pinCode);
-                long phoneNumber = sc.nextLong();
-                contactArray[i].setPhoneNumber(phoneNumber);
-                String email = sc.next();
-                contactArray[i].setEmailId(email);
+                System.out.print("Enter address,city,state,pincode,phonenumber,email");
+                String ad = sc.next();
+                contactArray[i].setAddress(ad);
+                String ci = sc.next();
+                contactArray[i].setCity(ci);
+                String st = sc.next();
+                contactArray[i].setState(st);
+                int pin = sc.nextInt();
+                contactArray[i].setPinCode(pin);
+                long num = sc.nextLong();
+                contactArray[i].setPhoneNumber(num);
+                String em = sc.next();
+                contactArray[i].setEmailId(em);
                 System.out.println(contactArray[i]);
                 break;
             }
@@ -70,24 +95,16 @@ public class AddressBookMain {
     }
 
     /**
-     * This is the main method that calls other methods
-     *
-     * @param args
+     * Delete contact details of a record of a
+     * person using their First Name.
      */
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter FirstName,Last Name,address,city,state,pinCode,phoneNumber,email");
-        String firstName = sc.next();
-        String lastName = sc.next();
-        String address = sc.next();
-        String cityNew = sc.next();
-        String state = sc.next();
-        int zip = sc.nextInt();
-        long phoneNumber = sc.nextLong();
-        String email = sc.next();
-        AddressBookMain Contact1 = new AddressBookMain();
-        Contact1.addNewContact(firstName, lastName, address, cityNew, state, zip, phoneNumber, email);
-        Contact1.editContact();
-        Contact1.printContact();
+    public void deleteContactDetails(String firstName, String lastName) {
+        for (int i = 0; i < numOfContact; i++) {
+            if (contactArray[i].firstName.equals(firstName) && contactArray[i].lastName.equals(lastName)) {
+                contactArray[i] = null;
+                System.out.println("Contact deleted");
+                break;
+            }
+        }
     }
 }
